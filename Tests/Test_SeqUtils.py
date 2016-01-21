2a735bd05413820c1cd8fa89360930b6b548e0dd
allista <allista@gmail.com>
2016-01-21 19:56:05 +0300

    Added SeqUtils and msgpack tests.

---
+++ Tests/Test_SeqUtils.py
@@ -0,0 +1,44 @@
+'''
+Created on Jan 15, 2016
+
+@author: allis
+'''
+from BioUtils.Tools.Multiprocessing import MPMain
+from BioUtils.Tools.Output import simple_timeit
+
+class _Main(MPMain):
+    large_seqdb = u'/home/allis/Downloads/Local/Bioinformatics/SILVA/SILVA_123_SSURef_Nr99_tax_silva.fasta'
+    #silva.gold.ng.fasta'#nogap.archaea.fasta' #SILVA_123_SSURef_Nr99_tax_silva.fasta'
+    def _main(self):
+        from BioUtils.SeqUtils import SeqView
+        from BioUtils.Tools.Multiprocessing import parallelize_work
+        
+        with simple_timeit('load'):
+            sv = SeqView()
+            sv.load([self.large_seqdb])
+            
+        ssv = sv.subview(sv.keys()[:5])
+        print ssv.keys()
+        print ssv[3]
+        print
+        
+        import cPickle as pickle
+        ssv1 = pickle.loads(pickle.dumps(ssv, protocol=-1))
+        print ssv1.keys()
+        print ssv1[3]
+        print
+            
+        def worker(r): return r.id    
+        for numrecs in xrange(1000, len(sv), 1000):
+            svs = sv[0:numrecs]
+            with simple_timeit('sequential %d' % numrecs):
+                res1 = [svs[i].id for i in range(numrecs)]
+            with simple_timeit('parallel %d' % numrecs):
+                res2 = parallelize_work(self.abort_event, 1, 1, worker, svs, copy_data=True)
+            assert res1 == res2
+            print '-'*80
+        print 'Done'
+
+if __name__ == '__main__':
+    main =_Main()
+    main()
---
+++ Tests/Test_msgpack.py
@@ -0,0 +1,32 @@
+'''
+Created on Jan 19, 2016
+
+@author: allis
+'''
+
+import msgpack
+import pickle
+from pydoc import locate
+from DegenPrimer.SecStructures import Duplex
+
+if __name__ == '__main__':
+    d1 = Duplex('ATGCGCTA', 'ATGCGCTA')
+    d2 = Duplex('ATGCGCTA', 'ATGCGCTA', revcomp=True)
+    anns = (123, (d1, d2), {4:'assdf'})
+
+    def packb_obj(obj):
+        if hasattr(obj, '__dict__'):
+            return dict(type=obj.__module__+'.'+type(obj).__name__, data=obj.__dict__)
+        elif hasattr(obj, '__slots__'):
+            return dict(type=obj.__module__+'.'+type(obj).__name__, data=dict((slot, getattr(obj, slot)) for slot in obj.__slots__))
+        else: return str(obj)
+            
+    def unpackb_obj(obj):
+        if 'type' in obj:
+            t = locate(obj['type'])
+            return t(**obj['data'])
+        return obj
+    
+    b = msgpack.packb(anns, default=packb_obj)
+    anns1 = msgpack.unpackb(b, use_list=False, object_hook=unpackb_obj)
+    print anns1
