015a2892737746840872e5e6bc43642640f6d338
allista <allista@gmail.com>
2016-03-17 10:04:27 +0300

    Corrected setup script.

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 from distutils.core import setup
-setup(name='seqconv',
-      version='1.3',
-      description='A set of utilities for bioinformatics.',
+setup(name='BioUtils',
+      version='1.4',
+      description='A utility library and a set of scripts for everyday bioinformatic routine.',
       long_description=read('README'),
       license='GPL-3',
       author='Allis Tauri',
@@ -31,7 +31,7 @@ setup(name='seqconv',
         'Operating System :: POSIX',
         'Programming Language :: Python'],
 #      install_require=['biopython>=1.6', 'dendropy>=4.0', 'numpy', 'scipy'],
-      packages=['BioUtils', 'BioUtils.HMMER3', 'BioUtils.NCBI', 'BioUtils.Tools'],
+      packages=['BioUtils', 'BioUtils.HMMER3', 'BioUtils.KEGG', 'BioUtils.NCBI', 'BioUtils.Tools'],
       scripts=['seqconv',
                'align_as_amino',
                'gaps_encoder',
