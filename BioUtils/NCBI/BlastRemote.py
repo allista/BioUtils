bf42eb229a677b6be09f57cbc847d2d3a3a38cd0
allista <allista@gmail.com>
2016-12-09 01:41:28 +0300

    Added BlastWWW.load_results classmethod; fixed results fetching.

--- BioUtils/NCBI/BlastRemote.py
+++ BioUtils/NCBI/BlastRemote.py
@@ -6,6 +6,8 @@ Created on Mar 17, 2016
 @author: Allis Tauri <allista@gmail.com>
 '''
 
+from Bio.Blast import NCBIXML
+
 from BioUtils.Tools.Output import ProgressCounter
 from BioUtils.Tools.Misc import ListDB
 
@@ -15,6 +17,16 @@ from .BlastBase import _BlastBase
 class BlastWWW(_BlastBase):
     '''Perform a blast search possibly restricted by Entrez query, then fetch
     results as sequences with annotations'''
+
+    @classmethod
+    def load_results(cls, filename):
+        try:
+            with open(filename) as inp:
+                results = list(NCBIXML.parse(inp))
+            return results
+        except Exception as e:
+            print 'Unhandled exception: %s' % str(e)
+            return None
     
 #    def __init__(self, job_id):
 #        self._job_id = job_id
@@ -129,43 +141,53 @@ class BlastWWW(_BlastBase):
 
     @classmethod
     def _fetch_query(cls, q, entrez):
-        if q.region:
-            records = entrez.get_records(q.term, q.db,
-                                         seq_start=q.region.start,
-                                         seq_end=q.region.end,
-                                         strand=q.entrez_strand)
-            return q.expand_subregions(records)
+        if q.subregions:
+            records = []
+            for r in q.subregions:
+                records.extend(entrez.get_records(q.term, q.db,
+                                                  seq_start=r.start,
+                                                  seq_stop=r.end,
+                                                  strand=r.entrez_strand))
+            return records
+        elif q.region:
+            return entrez.get_records(q.term, q.db,
+                                      seq_start=q.region.start,
+                                      seq_stop=q.region.end,
+                                      strand=q.region.entrez_strand)
         else: return entrez.get_records(q.term, q.db)
-    
+
     @classmethod
-    def fetch_results(cls, email, results, from_dbs=None, what='record', **kwargs):
-        if what not in cls._fetch_targets:
-            raise ValueError('"what" parameter may only be in: %s' % str(cls._fetch_targets))
+    def fetch_queries(cls, email, queries, from_dbs=None, **kwargs):
         if not email:
             raise ValueError('You should always provide a valid e-mail '
                              'to NCBI when performing an Entrez query.')
-        #search the ID of each blast result, then fetch corresponding part of the sequence
+        # search the ID of each blast result, then fetch corresponding part of the sequence
         batch = ListDB()
         single = ListDB()
         fetched = []
         be = BatchEntrez(email)
         with ProgressCounter('', 0, replace=False) as prg:
-            for record in results:
-                for alignment in record.alignments:
-                    q = cls.Query(alignment, what)
-                    if not q: continue
-                    if q.region: single[q.db] = q
-                    else: batch[q.db] = q
-                    prg.count()
+            for q in queries:
+                if q.region: single[q.db] = q
+                else: batch[q.db] = q
             for db in single:
                 if not from_dbs or db in from_dbs:
-                    for q in single[db]: 
+                    for q in single[db]:
                         fetched += cls._fetch_query(q, be)
                         prg.count()
             for db in batch:
                 if not from_dbs or db in from_dbs:
-                    fetched += be.get_records_for_terms(batch[db], db)
-                    prg.cound()
+                    fetched += be.get_records_for_terms([q.term for q in batch[db]], db)
+                    prg.count()
         return fetched
-    #end def
+
+    @classmethod
+    def fetch_results(cls, email, results, from_dbs=None, what='record', **kwargs):
+        if what not in cls._fetch_targets:
+            raise ValueError('"what" parameter may only be one of: %s' % str(cls._fetch_targets))
+        queries = []
+        for ali in cls.iter_alignments(results):
+            q = cls.Query(ali, what)
+            if q: queries.append(q)
+        return cls.fetch_queries(email, queries, from_dbs, **kwargs)
 #end class
