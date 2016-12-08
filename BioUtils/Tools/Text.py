c45a1b2ccf735ae01112ebef53605b4e764c0499
allista <allista@gmail.com>
2016-12-09 01:57:25 +0300

    Refactoring.

--- BioUtils/Tools/Text.py
+++ BioUtils/Tools/Text.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 
-'''
+"""
 Created on Jun 30, 2012
 
 @author: Allis Tauri <allista@gmail.com>
-'''
+"""
 
 import re
 import random
@@ -14,8 +14,10 @@ from time import ctime
 
 text_width = 80
 
+
 def random_text(length, alphabet=string.ascii_uppercase+string.digits):
     return ''.join(random.choice(alphabet) for _unused in xrange(length))
+
 
 def hr(s='', symbol='-', width=text_width):
     if len(s) > text_width-4:
@@ -23,8 +25,10 @@ def hr(s='', symbol='-', width=text_width):
     else: lines = [s]
     return '\n'.join((' %s ' % l).center(width, symbol) for l in lines if l) + '\n\n'
 
+
 def time_hr(symbol='#'):
     return hr(str(ctime()), symbol)
+
 
 def wrap_text(text, width=None):
     #replace intermediate whitespaces with single spaces
@@ -50,10 +54,11 @@ def wrap_text(text, width=None):
         curw += wl
     out += '\n'
     return out
-            
+
+
 def line_by_line(texts, widths, divider='|', filler=' ', j_down=False, j_center=False):
     assert len(texts) == len(widths), \
-    'line_by_line: for each text a line width should be provided'
+        'line_by_line: for each text a line width should be provided'
     _texts = tuple(wrap_text(t, widths[w]-1)[:-1].splitlines() for w,t in enumerate(texts))
     _lines = max(len(t) for t in _texts)
     output_text = ''
@@ -63,7 +68,7 @@ def line_by_line(texts, widths, divider='|', filler=' ', j_down=False, j_center=
                 if l < len(t):
                     if j_center:
                         output_text += t[l].center(widths[w]-1, filler) + divider
-                    else: 
+                    else:
                         output_text += t[l].ljust(widths[w]-1, filler) + divider
                 else: output_text += filler*(widths[w]-1) + divider
             else:
@@ -71,11 +76,12 @@ def line_by_line(texts, widths, divider='|', filler=' ', j_down=False, j_center=
                 if l >= l_start:
                     if j_center:
                         output_text += t[l-l_start].center(widths[w]-1, filler) + divider
-                    else: 
+                    else:
                         output_text += t[l-l_start].ljust(widths[w]-1, filler) + divider
                 else: output_text += filler*(widths[w]-1) + divider
         output_text += '\n'
     return output_text
+
 
 def print_dict(_dict, header=None, delimiter=':'):
     if not _dict: return ''
@@ -89,15 +95,16 @@ def print_dict(_dict, header=None, delimiter=':'):
     dict_str = ''
     for key, val in dict_items:
         dict_str += '%s%s %s%s %s\n' % (str(key),
-                                        ' '*(max_key_len-len(str(key))), 
+                                        ' '*(max_key_len-len(str(key))),
                                         delimiter,
                                         ' '*(max_value_len-len(str(val))),
                                         str(val))
     return dict_str
 
+
 def print_table(table, delimiter=':'):
     if not table: return ''
-    if len(set(len(row) for row in table)) > 1: 
+    if len(set(len(row) for row in table)) > 1:
         raise ValueError('StringTools.print_table: all rows in a table should be of equal size.')
     max_col_len = [max(len(str(table[r][c])) for r in xrange(len(table))) for c in xrange(len(table[0]))]
     table_str = ''
@@ -114,12 +121,13 @@ def print_table(table, delimiter=':'):
         table_str += '\n'
     return table_str
 
+
 def format_quantity(quantity, unit='U'):
-    '''Given quantity in units, return it's string representation using 
-    prefixes m, u, n, p, etc.'''
+    """Given quantity in units, return it's string representation using
+    prefixes m, u, n, p, etc."""
     if quantity is None or quantity < 0: return 'N/A  %s' % unit
     if quantity == 0: return '0.0  %s' % unit
-    mag = -1*log(quantity, 10)
+    mag = -log(quantity, 10)
     if       mag < 0:  return '%.1f  %s' % (quantity,      unit)
     if 0  <= mag < 3:  return '%.1f m%s' % (quantity*1e3,  unit)
     if 3  <= mag < 6:  return '%.1f u%s' % (quantity*1e6,  unit)
@@ -131,11 +139,13 @@ def format_quantity(quantity, unit='U'):
     return 'N/A  %s' % unit
 #end def
 
+
 def issingleletter(s, l=None):
     if not s: return False
     if len(s) == 1: return True
     if l is None: l = s[0]
-    return all(si==l for si in s)
+    return all(si == l for si in s)
+
 
 class FilenameParser(object):
     ext_re = re.compile(r'.*\.(\w+)$')
@@ -144,12 +154,12 @@ class FilenameParser(object):
     @staticmethod
     def strip_ext(name):
         return name[:name.rfind('.')]
-    
+
     @classmethod
     def get_ext(cls, filename):
         m = cls.ext_re.match(filename)
         return m.group(1) if m else ''
-    
+
     @classmethod
     def guess_schema(cls, filename):
         for schema in cls.schemas:
@@ -161,4 +171,3 @@ class FilenameParser(object):
     def schema(cls, filename, schema):
         if schema: return schema
         return cls.guess_schema(filename)
-
